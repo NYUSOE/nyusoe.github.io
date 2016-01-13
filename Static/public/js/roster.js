79504f143028504f820e8496730026cc096c6e64
Nicolai Ferraris <Nicolai@Nicolais-MBP-2.home>
2016-01-13 05:08:04 -0500

    More cleanup, added design around bios, bugfixes

--- Static/public/js/roster.js
+++ Static/public/js/roster.js
@@ -1,12 +1,16 @@
-var last;
+var last; //Variable that keeps track of last opened council member
 function toggler(id) {
-    var e = document.getElementById(id);
-    if(typeof last != 'undefined'){
-    	last.style.display = 'none';
-    }
-    last = e;
-    if (e.style.display == 'block')
+	var e = document.getElementById(id); //clicked element
+	if (e.style.display == 'block') //if item is displayed, then hide it
         e.style.display = 'none';
     else
-        e.style.display = 'block';
+        e.style.display = 'block'; //Else, item is hidden, then show it.
+    if(last == e){ //checks if trying to click on same person
+    	return; //exit
+    }
+    if(typeof last != 'undefined'){ //This checks if last is defined or not.
+    	last.style.display = 'none'; //if last is defined, then we hide last element
+    }
+
+    last = e; //resetting last
 }
